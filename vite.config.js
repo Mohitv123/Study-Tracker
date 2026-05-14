d71427b825605509fe31025290ec34cac44471aa
Mohit Venkatesh <mohitgvenkatesh@gmail.com>
2026-05-15 01:44:09 +0530

    Added PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      includeAssets: ['favicon.svg'],
+
+      manifest: {
+        name: 'Study Tracker',
+        short_name: 'Tracker',
+        description: 'Track study progress',
+
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+
+        display: 'standalone',
+        start_url: '/',
+
+        icons: [
+          {
+            src: '/favicon.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
+  ],
 })
